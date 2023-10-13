b12e45768d23aaac0f96f52e8e5249983c8f289d
kimmokyung <144295564+kimmokyung@users.noreply.github.com>
2023-10-13 11:35:37 +0900

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("hello world")
+hello world
+"hello world"
+'hello world'
+4+4
+8
+hello world
+SyntaxError: incomplete input
